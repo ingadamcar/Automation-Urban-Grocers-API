f2950bb37a882ae4f7e9e56b9c0e51dbc85b226f
Adam Uriel Cárdenas Padilla <ing.adam.cardenas@gmail.com>
2026-05-20 17:55:26 -0600

    Add demo video

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-752b7dcf-9897-42df-8b11-e46c320ccde5.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-47503d01-5ce5-4258-9aaf-c4bfc148a63c.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"
